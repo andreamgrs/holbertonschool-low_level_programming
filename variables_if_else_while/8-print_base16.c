7907096a6d71a052b4bb7a1b3daadf645f5b3785
andreamgrs <andrea.munozgr@gmail.com>
2025-06-19 03:06:46 +0000

    Adding script 9

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Code that prints the alphabet in lowercase and uppercase.
+ * main - Code that prints all the numbers of base 16 in lowercase.
  *
  *
  * Return: Always 0 (Success)
---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Code prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number = 0;
+	int final_number = 9;
+
+	while (number <= final_number)
+	{
+		putchar('0' + number);
+
+		if (number != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		number = number + 1;
+	}
+	putchar('\n');
+	return(0);
+}
