5e0e5e63d1f47d09d51a476c993adedd3f8a6cb7
andreamgrs <andrea.munozgr@gmail.com>
2025-06-19 00:35:25 +0000

    Adding script 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Code prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
