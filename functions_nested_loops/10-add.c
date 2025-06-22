ac8837c00cdedc3caa2eead3197a3bb19cd80051
andreamgrs <andrea.munozgr@gmail.com>
2025-06-22 09:51:15 +0000

    Adding 10-main, prototype and script 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result.
+ *
+ * @a: the integer to evaluate.
+ * @b: the integer to evaluate.
+ *
+ * Return: the result of the 2 integers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
