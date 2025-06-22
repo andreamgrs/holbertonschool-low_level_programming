cc2044fd6146032b5481294a79eb63e1f0d4320f
andreamgrs <andrea.munozgr@gmail.com>
2025-06-22 09:44:15 +0000

    Adding 9-main, prototype and script 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,6 +2,7 @@
 /**
  * times_table - function that prints the 9 times table.
  *
+ *i for rows the number being multiplied j for columns the table.
  *
  * Return: void.
  */
@@ -13,26 +14,37 @@ void times_table(void)
 	int product;
 	int first_num;
 	int second_num;
+
 	for (i = 0; i <= n; i++)
 	{
-		for(j = 0; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			product = i * j;
 			first_num = product / 10;
 			second_num = product % 10;
-			_putchar(' ');
 
-			if (first_num == 0)
+			if (j != 0)
 			{
+				if (first_num == 0)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(first_num + '0');
+				}
+			}
+			_putchar(second_num + '0');
+
+			if (j < 9)
+			{
+				_putchar(',');
 				_putchar(' ');
 			}
 			else
 			{
-				_putchar(first_num + '0');
+				_putchar('\n');
 			}
-			_putchar(second_num + '0');
-			_putchar(44);
 		}
-		_putchar('\n');
 	}
 }
