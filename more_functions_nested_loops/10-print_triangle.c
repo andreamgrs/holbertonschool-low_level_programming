df47c52b0e4d6bdf45466504565e61601a28d470
andreamgrs <andrea.munozgr@gmail.com>
2025-06-23 12:16:55 +0000

    Adding 10-main, prototype and script 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - function that prints a triangle.
+ *
+ * @size: is the size of the triangle
+ *
+ * Return: triangle in #. If is 0 or less, only print \n.
+ */
+void print_triangle(int size)
+{
+	int current_num;
+	int more_squares;
+
+	for (current_num = 0; current_num < size; current_num++)
+	{
+		for (more_squares = 0; more_squares < size; more_squares++)
+		{
+			_putchar(' ');
+		}
+
+		_putchar(35);
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
