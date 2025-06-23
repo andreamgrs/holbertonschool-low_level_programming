10b7bc02f309fd09dc264e14d04332ddce79d118
andreamgrs <andrea.munozgr@gmail.com>
2025-06-23 02:23:42 +0000

    Adding prototype, 2-main and script 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul -  multiplies two integers and returns the result.
+ *
+ * @a: the integer to evaluate.
+ * @b: the integer to evaluate.
+ *
+ * Return: the multiplication of the 2 integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
