3e1311f9a4d63f53496357a21b3421db0da80dd9
andreamgrs <andrea.munozgr@gmail.com>
2025-06-25 02:54:37 +0000

    Adding 3-main and script 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ *
+ * @str: pointer that points to a string.
+ *
+ * Return: prints the string.
+ */
+void _puts(char *str)
+{
+	int cont;
+
+	while (str[cont] != 0)
+	{
+		_putchar(str[cont]);
+		cont = cont + 1;
+	}
+	_putchar('\n');
+}
