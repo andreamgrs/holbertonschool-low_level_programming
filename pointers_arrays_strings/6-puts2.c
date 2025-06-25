7c28d3213a764ccc9a0528e2f80692187f8567e0
andreamgrs <andrea.munozgr@gmail.com>
2025-06-25 04:02:40 +0000

    Adding 6-main and script 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - prints every character of a string, starting with the first character.
+ * puts2 - prints every character of a string.
  *
  * @str: pointer that points to a string.
  *
@@ -9,17 +9,17 @@
 void puts2(char *str)
 {
 	int cont = 0;
-	int tmp = 0;
+	int tmp;
 
 	while (str[cont] != 0)
 	{
 		cont = cont + 1;
 	}
-	
+	tmp = 0;
 	while (tmp <= cont)
 	{
 		_putchar(str[tmp]);
 		tmp = tmp + 2;
-	}	
+	}
 	_putchar('\n');
 }
