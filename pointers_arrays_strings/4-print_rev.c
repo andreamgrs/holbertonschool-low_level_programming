c051b415e0f737541ab3dc6546e509d5514eb0d5
andreamgrs <andrea.munozgr@gmail.com>
2025-06-25 03:21:11 +0000

    Adding 4-main and script 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse.
+ *
+ * @s: pointer that points to a string.
+ *
+ * Return: prints the reverse string.
+ */
+void print_rev(char *s)
+{
+	int cont = 0;
+	int tmp;
+
+	while (s[cont] != 0)
+	{
+		cont = cont + 1;
+	}
+	tmp = cont - 1;
+	while (tmp != 0)
+	{
+		_putchar(s[tmp]);
+		tmp = tmp - 1;
+	}
+	_putchar('\n');
+}
