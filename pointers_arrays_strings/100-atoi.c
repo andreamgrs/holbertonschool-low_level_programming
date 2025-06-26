427c52490bb9cc0c8262aacbba58c30d38d1ed5c
andreamgrs <andrea.munozgr@gmail.com>
2025-06-26 00:21:33 +0000

    Adding 10-main and script 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+ #include "main.h"
+/**
+ * _atoi - convert a string to an integer.
+ *
+ * @s: pointer that points to a char string.
+ *
+ * Return: the string converted into an integer.
+ */
+int _atoi(char *s)
+{
+}
