4397a2f218460aed22d9d88ac2909bd42312b933
andreamgrs <andrea.munozgr@gmail.com>
2025-06-27 04:18:09 +0000

    Adding 3-main and script 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,17 +11,19 @@ int _strcmp(char *s1, char *s2)
 {
 	int cont;
 	int result = 0;
+	int diff_find = 0;
 
 	cont = 0;
-	while (s1[cont] != '\0' && s2[cont] != '\0')
+	while (s1[cont] != '\0' && s2[cont] != '\0' && diff_find == 0)
 	{
-		if (s1[cont] == s2[cont])
+		if (s1[cont] != s2[cont])
 		{
-			result = 0;
+			result = s1[cont] - s2[cont];
+			diff_find = 1;
 		}
 		else
 		{
-			result = s1[cont] - s2[cont];
+			result = 0;
 		}
 	cont = cont + 1;
 	}
