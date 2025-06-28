5f23754181c81385ab423ff73dadd1705b859d2a
andreamgrs <andrea.munozgr@gmail.com>
2025-06-28 06:49:15 +0000

    Adding 7-main and script 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,19 +9,19 @@
 char *leet(char *s)
 {
 	int cont;
-	int array_cont;
-	int array_upper[] = {65, 69, 79, 84, 76};
-	int array_lower[] = {97, 101, 111, 116, 108};
-	int array_num[] = {52, 51, 48, 55, 49};
+	int a_cont;
+	int a_upper[] = {65, 69, 79, 84, 76};
+	int a_lower[] = {97, 101, 111, 116, 108};
+	int a_num[] = {52, 51, 48, 55, 49};
 
 	cont = 0;
 	while (s[cont] != '\0')
 	{
-		for (array_cont = 0; array_cont < 5; array_cont++)
+		for (a_cont = 0; a_cont < 5; a_cont++)
 		{
-			if (s[cont] == array_upper[array_cont] || s[cont] == array_lower[array_cont])
+			if (s[cont] == a_upper[a_cont] || s[cont] == a_lower[a_cont])
 			{
-				s[cont] = array_num[array_cont];
+				s[cont] = a_num[a_cont];
 			}
 		}
 		cont = cont + 1;
