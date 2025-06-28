ed3039f849ac55cb63ce21d64a1d28e01310fdb1
andreamgrs <andrea.munozgr@gmail.com>
2025-06-28 05:41:51 +0000

    Adding 6-main and script 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,8 @@ char *cap_string(char *s)
 {
 	int cont = 0;
 	int aux = 0;
+	int array[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int cont_array;
 
 	if (s[cont] >= 'a' && s[cont] <= 'z')
 	{
@@ -18,12 +20,15 @@ char *cap_string(char *s)
 
 	while (s[cont] != '\0')
 	{
-		if (s[cont] == 32 || s[cont] == 9 || s[cont] == 10 || s[cont] == 44 || s[cont] == 59 || s[cont] == 46 || s[cont] == 33 || s[cont] == 63 || s[cont] == 34 || s[cont] == 40 || s[cont] == 41 || s[cont] == 123 || s[cont] == 125)
+		aux = cont + 1;
+		for (cont_array = 0; cont_array < 13; cont_array++)
 		{
-			aux = cont + 1;
-			if (s[aux] >= 'a' && s[aux] <= 'z')
+			if (s[aux] == array[cont_array])
 			{
-				s[aux] = s[aux] - 32;
+				if (s[aux] >= 'a' && s[aux] <= 'z')
+				{
+					s[aux] = s[aux] - 32;
+				}
 			}
 		}
 		cont = cont + 1;
