ce7c83a92437851a2aa3383564532f6130d5f6e8
andreamgrs <andrea.munozgr@gmail.com>
2025-06-30 06:29:00 +0000

    Adding 4-main and script 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strpbrk -  function that searches a string for any of a set of bytes.
+ *
+ * @s: pointer to the string.
+ * @accept: characters to find.
+ *
+ * Return: pointer to the byte in s that matches one of the bytes in accept.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int cont = 0;
+	int cont2;
+
+	while (s[cont] != '\0')
+	{
+		cont2 = 0;
+		while (accept[cont2] != '\0')
+		{
+			if (s[cont] == accept[cont2])
+			{
+				return (s + cont);
+			}
+			cont2 = cont2 + 1;
+		}
+		cont = cont + 1;
+	}
+	return (0);
+	
+}
