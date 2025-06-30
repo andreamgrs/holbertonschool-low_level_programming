a7e0668447d535c752dba1c2cfc9d5eceec52ffb
andreamgrs <andrea.munozgr@gmail.com>
2025-06-30 03:37:35 +0000

    Adding o-main and script 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with a constant byte.
+ *
+ * @s: var that point where, is the array. 
+ * @b: contstant byte that we use to fill.
+ * @n: first n bytes of the memory area that we will fill.
+ *
+ * Return: pointer that points the buffer with the new constant byte.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int cont = 0;
+
+	while (cont < n)
+	{
+		s[cont] = b;
+		cont = cont + 1;
+	}
+	return (s);
+}
