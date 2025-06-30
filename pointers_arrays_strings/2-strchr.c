d5ba4cef6ad5b07b38e86aa2ef9d3d32eefd7e5e
andreamgrs <andrea.munozgr@gmail.com>
2025-06-30 05:14:21 +0000

    Adding 2-main and script 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string.
+ *
+ * @s: pointer to the string.
+ * @c: character to find.
+ *
+ * Return: pointer that points to the string from the character or null.
+ */
+char *_strchr(char *s, char c)
+{
+	int cont = 0;
+
+
+	while (s[cont] != '\0')
+	{
+		if (s[cont] == c)
+		{
+			return (s + cont);
+		}
+		cont = cont + 1;
+	}
+	return ('\0');
+}
