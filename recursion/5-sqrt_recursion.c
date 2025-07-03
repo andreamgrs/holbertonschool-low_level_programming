118b7560fb4857744fd5b985fcf68e51850719e5
andreamgrs <andrea.munozgr@gmail.com>
2025-07-03 11:39:40 +0000

    Adding main and script 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _sqrt_aux - function to have an auxiliar num to guess the square number.
+ * _sqrt_recursion - function that returns the natural square root of a number.
+ *
+ * @n: number.
+ * @aux: the iterator.
+ *
+ * Return: natural square root of a number.
+ */
+int _sqrt_aux(int n, int aux)
+{
+	if (aux * aux == n)
+	{
+		return (aux);
+	}
+	else if (aux * aux > n)
+	{
+		return (-1);
+	}
+	return (_sqrt_aux(n, aux + 1));
+}
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	return (_sqrt_aux(n, 0));
+}
