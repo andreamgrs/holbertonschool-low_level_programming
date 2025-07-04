7a32e62b45f8cc1903b0f4c6f59fe275e1a047b4
andreamgrs <andrea.munozgr@gmail.com>
2025-07-04 02:00:41 +0000

    Adding main and script 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -3,6 +3,8 @@
  * is_prime_number - function that returns 1 if the input
  * integer is a prime number, otherwise return 0.
  *
+ * aux_num - function that detects if a num is prime with an aux num.
+ *
  * @n: number to evaluate.
  * @aux: number to interate.
  *
@@ -10,24 +12,20 @@
  */
 int aux_num(int n, int aux)
 {
-	if (aux > n)
-	{
-		return 0;
-	}
 	if (n % aux == 0)
 	{
-		return (1);
+		if (aux == n)
+		{
+			return (1);
+		}
+		return (0);
 	}
 	return (aux_num(n, aux + 1));
 
 }
 int is_prime_number(int n)
 {
-	if(n < 0)
-	{
-		return (0);
-	}
-	else if (n == 1)
+	if (n <= 1)
 	{
 		return (0);
 	}
