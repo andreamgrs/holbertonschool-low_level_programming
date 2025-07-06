9f37a66208a1b154b6a2322824e30fab4764d9ee
andreamgrs <andrea.munozgr@gmail.com>
2025-07-06 04:48:48 +0000

    Adding main and script 4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -15,7 +15,8 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	int cont_arg;
 	int cont_vec;
-	for (cont_arg = 1; cont_arg < argc; cont_arg ++)
+
+	for (cont_arg = 1; cont_arg < argc; cont_arg++)
 	{
 		cont_vec = 0;
 		while (argv[cont_arg][cont_vec] != '\0')
@@ -26,9 +27,9 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 			cont_vec = cont_vec + 1;
-                }
-                sum = sum + atoi(argv[cont_arg]);
-        }
-        printf("%d\n", sum);
-        return (0);
+		}
+		sum = sum + atoi(argv[cont_arg]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
