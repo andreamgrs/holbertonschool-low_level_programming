1206dba9eaaa1fd83d4e841c862ea5e6f8e62c27
andreamgrs <andrea.munozgr@gmail.com>
2025-07-09 12:32:17 +0000

    Adding main and script 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - function that allocates memory using malloc.
+ *
+ * @b: the size of the allocate memory.
+ *
+ * Return: returns a pointer to the allocated memory.
+ * Is void* not only void.
+ */
+void *malloc_checked(unsigned int b)
+{
+        void *p;
+        p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
