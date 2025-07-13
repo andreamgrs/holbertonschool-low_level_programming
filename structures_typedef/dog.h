2a81d27827c8428b3bf667127938fe9b39b66d5e
andreamgrs <andrea.munozgr@gmail.com>
2025-07-13 04:45:52 +0000

    Adding 0-main, README.md and dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - struct that stores information about dog.
+ *
+ * @name: name of dog.
+ * @age: age of dog.
+ * @owner: owner of dog.
+ *
+ */
 struct dog
 {
 	const char *name;
