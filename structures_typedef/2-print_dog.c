16fbbcf5367a149b339631d69db11c384827ae00
andreamgrs <andrea.munozgr@gmail.com>
2025-07-13 05:35:22 +0000

    Adding main and script 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,9 +17,17 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: (nil)\n");
 	}
- 	if (d->owner == NULL)
-        {
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+	{
 		printf("Owner: (nil)\n");
-        }
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+	else
+	{
+		 printf("Owner: %s\n", d->owner);
+	}
 }
