778dd7ac4c79ab1985b86151f27dd4c81bc4c4f9
andreamgrs <andrea.munozgr@gmail.com>
2025-07-17 07:53:27 +0000

    Adding corrections on task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -24,6 +24,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	operation = get_op_func(argv[2]);
 	result = operation(a, b);
 
