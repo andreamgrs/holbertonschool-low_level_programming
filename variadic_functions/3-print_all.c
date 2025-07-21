326ff04661520a01f320cd5c6e1e107ecb1c2ec3
andreamgrs <andrea.munozgr@gmail.com>
2025-07-21 05:13:05 +0000

    Adding main and script 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,7 +4,8 @@
 #include <stdio.h>
 
 /**
- * print_char - print a char.
+ * print_char - function that prints a char.
+ *
  * @arg: A list of arguments.
  */
 void print_char(va_list arg)
@@ -16,37 +17,40 @@ void print_char(va_list arg)
 }
 
 /**
- * print_int - print an integer.
+ * print_int - function that prints an integer.
+ *
  * @arg: A list of arguments.
  */
 void print_int(va_list arg)
 {
-        int i;
-
-        i = va_arg(arg, int);
-        printf("%d", i);
+	int i;
+	
+	i = va_arg(arg, int);
+	printf("%d", i);
 }
 
 /**
- * print_float - print a float
+ * print_float - print a float.
+ *
  * @arg: A list of arguments.
  */
 void print_float(va_list arg)
 {
-        float f;
-
-        f = va_arg(arg, double);
-        printf("%f", f);
+	float f;
+	
+	f = va_arg(arg, double);
+	printf("%f", f);
 }
 
 /**
  * print_string - print a string.
+ *
  * @arg: A list of arguments.
  */
 void print_string(va_list arg)
 {
-        char *s;
-
+	char *s;
+	
 	s = va_arg(arg, char *);
 
 	if (s == NULL)
@@ -54,15 +58,14 @@ void print_string(va_list arg)
 		printf("(nil)");
 		return;
 	}
-	
-        printf("%s", s);
+	printf("%s", s);
 }
 /**
  * print_all - function that prints anything.
  *
  * @format: list of types of arguments passed to the function.
  * c: char, i: int, f: float, s: char *
- * @n: number of strings passed to the function.
+ * @...: a variable number or arguments.
  *
  * Return: nothing.
  */
@@ -73,11 +76,11 @@ void print_all(const char * const format, ...)
 	va_list args;
 
 	op_list ops[] = {
-        {"c", print_char},
-        {"i", print_int},
-        {"f", print_float},
-        {"s", print_string},
-        {NULL, NULL}
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
 	};
 	
 	va_start(args, format);
