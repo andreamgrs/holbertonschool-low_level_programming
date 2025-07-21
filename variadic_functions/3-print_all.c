f720705c849e9eb2156ee3e5d7a1f437a8a2c4ce
andreamgrs <andrea.munozgr@gmail.com>
2025-07-21 03:34:49 +0000

    Adding main and script 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,6 +2,59 @@
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdio.h>
+
+/**
+ * print_char - print a char.
+ * @arg: A list of arguments.
+ */
+void print_char(va_list arg)
+{
+	char c;
+
+	c = va_arg(arg, char);
+	printf("%c", c);
+}
+
+/**
+ * print_int - print an integer.
+ * @arg: A list of arguments.
+ */
+void print_int(va_list arg)
+{
+        int i;
+
+        i = va_arg(arg, int);
+        printf("%d", i);
+}
+
+/**
+ * print_float - print a float
+ * @arg: A list of arguments.
+ */
+void print_float(va_list arg)
+{
+        float f;
+
+        f = va_arg(arg, double);
+        printf("%f", f);
+}
+
+/**
+ * print_string - print a string.
+ * @arg: A list of arguments.
+ */
+void print_char(va_list arg)
+{
+        char *s;
+
+	if (s == NULL)
+	{
+		printf("(nil)");
+	}
+
+        s = va_arg(arg, char *);
+        printf("%s", s);
+}
 /**
  * print_all - function that prints anything.
  *
@@ -15,12 +68,15 @@ void print_all(const char * const format, ...)
 {
 	unsigned int cont_format, cont;
 	char *s;
-	int i;
-	char c;
-	float f;
-
 	va_list args;
-
+	op_list ops[] = {
+        {"c", print_char},
+        {"i", print_int},
+        {"f", print_float},
+        {"s", print_string},
+        {NULL, NULL}
+	};
+	
 	va_start(args, format);
 
 	cont_format = 0;
@@ -30,20 +86,7 @@ void print_all(const char * const format, ...)
 	}
 	while (cont < cont_format)
 	{
-		s = va_arg(args, char *);
-		if (cont > 0 && separator != NULL)
-		{
-			printf("%s", separator);
-		}
-
-		if (string == NULL)
-		{
-			printf("(nil)");
-		}
-		else
-		{
-			printf("%s", string);
-		}
+		ops[cont].list(args);
 		cont = cont + 1;
 	}
 	printf("\n");
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,18 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+/**
+ * op - Struct option to print
+ *
+ * @op - option for the data type. 
+ * c: char, i: int, f: float, s: char *
+ * @print: The function associated, which function to print.
+ */
+typedef struct op
+{
+    char *op;
+    void (*print)(va_list arg);
+} op_list;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
