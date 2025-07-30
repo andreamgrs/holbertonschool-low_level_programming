eeab457784f531e638187798857e046c666730ae
andreamgrs <andrea.munozgr@gmail.com>
2025-07-30 06:47:27 +0000

    Adding task and main 1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,30 +1,21 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * print_list - function that prints all the elements of a list_t list.
+ * list_len - function that prints all the elements of a list_t list.
  *
  * @h: pointer h that points to the struct list_h.
  *
  * Return: number of nodes.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t num_nodes = 0;
 	const list_t *current = h;
 
 	while (current != NULL)
 	{
-		if (current->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%d] %s\n", current->len, current->str);
-		}
 		num_nodes = num_nodes + 1;
 		current = current->next;
 	}
-
 	return (num_nodes);
 }
