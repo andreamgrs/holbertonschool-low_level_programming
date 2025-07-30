6504f4bed9bd3f573c9a7ed92bd968373b778fff
andreamgrs <andrea.munozgr@gmail.com>
2025-07-30 06:34:48 +0000

    Adding task and main 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * print_list - function that prints all the elements of a list_t list.
  *
@@ -9,11 +10,17 @@
 size_t print_list(const list_t *h)
 {
 	size_t num_nodes = 0;
+	const list_t *current = h;
 
-	while (h != NULL)
+	while (current != NULL)
 	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		printf("[%d] %s\n", current->len, current->str);
 		num_nodes = num_nodes + 1;
-		h = h->next;
+		current = current->next;
 	}
 
 	return (num_nodes);
