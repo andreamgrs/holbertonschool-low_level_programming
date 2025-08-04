0e646d4fa58e542c7e561cdcd0fc10f0c7e6f7ce
root <andrea.munozgr@gmail.com>
2025-08-04 02:42:13 +0000

    Adding main 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+/**
+ * main - program that copies the content of a file to another file.
+ *
+ * Return: Always 0.
+ */
+int main(int cont_arg, char **av)
+{
+	int fd, fd2, r, w;
+	char buf[1024];
+	/**
+	 * av[0] cp
+	 * av[1] file from 
+	 * av[2] file to
+	 */
+	if (cont_arg != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file: %s\n", av[1]);
+                exit(98);
+	}
+	r = read(fd, buf, 1024);
+
+	fd2 = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(2, "Error: Can't write to: %s\n", av[2]);
+                exit(99);
+	}
+	w = write(fd2, buf, r);
+	if (r != w)
+	{
+		dprintf(2, "Error: Can't write to: %s\n", av[2]);
+                exit(99);
+	}
+	if (close(fd) == -1)
+	{
+		dprintf(2, "Error: Can't close fd: %d\n", fd);
+		exit(100);
+        }
+	if (close(fd2) == -1)
+	{
+		dprintf(2, "Error: Can't close fd2: %d\n", fd2);
+                exit(100);
+	}
+	return (0);
+}
