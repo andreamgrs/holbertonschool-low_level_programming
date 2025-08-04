ab405ca96632a39478bc86da24f4d1865e722808
root <andrea.munozgr@gmail.com>
2025-08-04 02:51:10 +0000

    Adding main 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -30,15 +30,19 @@ int main(int cont_arg, char **av)
                 exit(98);
 	}
 	r = read(fd, buf, 1024);
-
-	fd2 = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+	if (r == -1)
+	{
+		dprintf(2, "Error: Can't read from file: %s\n", av[1]);
+                exit(98);
+	}
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	if (fd2 == -1)
 	{
 		dprintf(2, "Error: Can't write to: %s\n", av[2]);
                 exit(99);
 	}
 	w = write(fd2, buf, r);
-	if (r != w)
+	if (w == -1)
 	{
 		dprintf(2, "Error: Can't write to: %s\n", av[2]);
                 exit(99);
