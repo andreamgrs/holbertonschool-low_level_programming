378b6f57b9f315f05c37de1237ffea760a986ba5
andreamgrs <andrea.munozgr@gmail.com>
2025-08-13 06:28:25 +0000

    Adding prompt.c

--- PID_PPID/strtok.c
+++ PID_PPID/strtok.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <string.h>
 
+int main(void)
+{
+	char text[] = "Hello! How; are;you?";
+	char *token = strtok(text, ";");
+
+	while (token != NULL)
+	{
+		printf("Token: %s\n", token);
+		token = strtok(NULL, ";");
+	}
+	return (0);
+}
