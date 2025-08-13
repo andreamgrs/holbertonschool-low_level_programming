fb39b2dad885a4e82acef97c48e29d4e28ee9d1b
andreamgrs <andrea.munozgr@gmail.com>
2025-08-13 06:29:59 +0000

    Adding strtok.c

--- PID_PPID/strtok.c
+++ PID_PPID/strtok.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - function that splits a string and
+ * returns an array of each word of the string. (strtok)
+ *
+ * Return: nothing.
+ *
+ */
 int main(void)
 {
 	char text[] = "Hello! How; are;you?";
